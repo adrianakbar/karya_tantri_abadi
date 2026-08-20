2112335d634c4dea564ff98e967c64226464c269
adrianakbar <armojkece123@gmail.com>
2026-08-20 16:44:13 +0700

    Fix card dobel: hapus getHeaderWidgets() override, widget didaftar sekali di panel provider

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -3,26 +3,8 @@
 namespace App\Filament\Pages;
 
 use Filament\Pages\Dashboard as BaseDashboard;
-use Filament\Widgets\AccountWidget;
-use Filament\Pages\Concerns\HasWidgets;
 
 class Dashboard extends BaseDashboard
 {
-    protected static ?string $navigationIcon = 'heroicon-o-home';
-
-    protected function getHeaderWidgets(): array
-    {
-        return [
-            \App\Filament\Widgets\RekapKeuanganStats::class,
-            \App\Filament\Widgets\RekapPinjamanStats::class,
-            // Inventory/POS dinonaktifkan untuk Karya Tantri Abadi
-            // \App\Filament\Widgets\InventoryOverview::class,
-            // \App\Filament\Widgets\RekapPenjualan::class,
-            \App\Filament\Widgets\RekapAnggota::class,
-            \App\Filament\Widgets\RekapAnggotaBaru::class,
-            \App\Filament\Widgets\RekapPinjaman::class,
-            \App\Filament\Widgets\RekapSimpanan::class,
-            \App\Filament\Widgets\RecentLoanRequests::class,
-        ];
-    }
+    protected static ?string $navigationIcon = "heroicon-o-home";
 }
